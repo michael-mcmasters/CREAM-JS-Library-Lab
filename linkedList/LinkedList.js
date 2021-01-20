68aa0038057ffa59339510ab0d25e4f9fadf26b6
michael-mcmasters <jmcmasters411@gmail.com>
2021-01-20 10:44:59 -0500

    Create LinkedList methods and one test

--- linkedList/LinkedList.js
+++ linkedList/LinkedList.js
@@ -1,5 +1,43 @@
 class LinkedList {
+  constructor(head = null) {
+    this.head = head;
+  }
 
+  add(element) {
+
+  }
+
+  addAt(index) {
+
+  }
+
+  remove() {
+
+  }
+
+  removeAt() {
+
+  }
+
+  contains() {
+
+  }
+
+  size() {
+
+  }
+
+  clear() {
+
+  }
+
+  getLast() {
+
+  }
+
+  getFirst() {
+
+  }
 }
 
 module.exports = LinkedList;
--- linkedList/LinkedList.test.js
+++ linkedList/LinkedList.test.js
@@ -2,4 +2,14 @@ const { TestScheduler } = require('jest');
 const LinkedList = require('./LinkedList');
 
 test("First Test", () => {
+  // Given
+  const linkedList = new LinkedList();
+
+  // When
+  linkedList.add(1);
+  const expected = true;
+  const actual = linkedList.contains(1);
+
+  // Then
+  expect(expected).toEqual(actual);
 });
